500a89837038e1183c1f651f1697d97c97754a73
Markus Schuettler <markus.schuettler@tngtech.com>
2026-04-24 11:10:45 +0200

    wip: add stars

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <script setup>
 import TicTacToe from './components/TicTacToe.vue'
+import StarsBackground from './components/StarsBackground.vue'
 </script>
 
 <template>
-  <main>
-    <TicTacToe />
-  </main>
+  <StarsBackground class="app-bg">
+    <main>
+      <TicTacToe />
+    </main>
+  </StarsBackground>
 </template>
 
 <style scoped>
+.app-bg {
+  min-height: 100vh;
+}
+
 main {
+  position: relative;
   display: flex;
   justify-content: center;
   padding: 2rem 0;
+  z-index: 1;
 }
 </style>
